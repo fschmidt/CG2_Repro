f12c75658114621ee171d0c3d687a71ff28c7fd7
mailtorico@googlemail.com <mailtorico@googlemail.com>
2011-10-27 12:50:58 +0200

    space

--- CG2-Raytracer/src/cg2/raytracerApp/Raytracer.java
+++ CG2-Raytracer/src/cg2/raytracerApp/Raytracer.java
@@ -7,11 +7,6 @@ public class Raytracer implements Painter {
 
 	@Override
 	public Color pixelColorAt(int x, int y, int width, int height) {
-<<<<<<< HEAD
-=======
-		
-		
->>>>>>> 2dfefeb1b60f4363847bcf88df0433f789c506e5
 		return null;
 	}
 }
